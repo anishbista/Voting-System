a031b825d050eafb947e02e222719b21ab22e2d4
benup211 <benup211@gmail.com>
2023-12-06 20:34:40 +0545

    Finished:Module Candidate Registered

--- file_handling.py
+++ file_handling.py
@@ -4,11 +4,11 @@ import json
 def read_file(file):
     with open(file, "r") as f:
         try:
-            return json.lead(f)
+            return json.load(f)
         except:
             return {}
 
 
 def write_file(obj, file):
     with open(file, "w") as f:
-        json.dump(obj, f)
+        json.dump(obj, f,indent=4)
